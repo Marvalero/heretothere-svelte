44efdd3e2690eb24ea185a6f5d751fb41b4d9ef0
Maria Valero <maria.valero@thoughtworks.com>
2022-02-08 19:49:06 +0100

    Add a routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,65 @@
 <script>
-	import HereToThere from './pages/HereToThere.svelte'
-	export let name;
+    import HereToThere from './pages/HereToThere.svelte';
+    import SecretHitler from './secretHitler/SecretHitlerPage.svelte';
+    import {Router, Route, Link} from "svelte-routing";
+    import Home from "./Home.svelte";
+
+    export let url = ""; //This property is necessary declare to avoid ignore the Router
+    export let name;
 </script>
 
 <main>
-	<HereToThere></HereToThere>
+    <Router url="{url}">
+        <nav>
+            <div class="link">
+                <Link to="/">Home</Link>
+            </div>
+            <div class="link">
+                <Link to="here-to-there">Here to There</Link>
+            </div>
+            <div class="link">
+                <Link to="secret-hitler">Secret Hitler</Link>
+            </div>
+        </nav>
+        <div>
+            <Route path="here-to-there" component="{HereToThere}"/>
+            <Route path="secret-hitler" component="{SecretHitler}"/>
+            <Route path="/">
+                <Home/>
+            </Route>
+        </div>
+    </Router>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    nav {
+        display: flex;
+    }
+    .link {
+        width: 100px;
+        background: pink;
+        padding: 10px;
+        margin: 10px;
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
---
+++ src/Home.svelte
@@ -0,0 +1,9 @@
+<div class="home-wrapper">
+    <h3>Choose a game!</h3>
+</div>
+<style>
+    .home-wrapper {
+        background: antiquewhite;
+        height: 500px;
+    }
+</style>
